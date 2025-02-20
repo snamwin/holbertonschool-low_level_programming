4e8e8310818fba310c1648435b746d5e26910bd7
snamwin <snamgodwin@yahoo.fr>
2025-02-20 13:49:08 +0100

    jetfyuy59

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,5 +1,10 @@
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
+/* Header includes prototype functions */
+
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
