dc6adbc4203f66f8cf70da60188ae17cad911a67
snamwin <snamgodwin@yahoo.fr>
2025-02-20 13:30:23 +0100

    jetfyuyj555

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,43 +1,28 @@
-#include "holberton.h"
-
+#include "main.h"
 /**
-  * times_table - Prints the 9 times table, starting with 0
-  *
-  * Return: empty output
-  */
+*times_table - prints the 9 times table
+*
+*Return: returns nothing
+*/
 void times_table(void)
 {
-	int x, y, z, u, d;
+int digit, mult, result;
+for (digit = 0; digit <= 9; digit++)
+{
+_putchar('0');
 
-	for (x = 0; x <= 9; x++)
-	{
-		for (y = 0; y <= 9; y++)
-		{
-			z = x * y;
+for (mult = 1; mult <= 9; mult++)
+{
+_putchar(',');
+_putchar(' ');
+result = digit * mult;
+if (result <= 9)
+_putchar(' ');
+else
+_putchar((result / 10) + '0');
 
-			if (z > 9)
-			{
-				u = z % 10;
-				d = (z - u) / 10;
-
-				_putchar(44);
-				_putchar(32);
-				_putchar(d + '0');
-				_putchar(u + '0');
-			}
-			else
-			{
-				if (y != 0)
-				{
-					_putchar(44);
-					_putchar(32);
-					_putchar(32);
-				}
-
-				_putchar(z + '0');
-			}
-		}
-
-		_putchar('\n');
-	}
+_putchar((result % 10) + '0');
+}
+_putchar('\n');
+}
 }
